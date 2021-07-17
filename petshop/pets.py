ba5c371741d95dc1c24028b623d966bd00453a1b
aksharabiju21 <84831335+aksharabiju21@users.noreply.github.com>
2021-07-18 00:46:43 +0530

    Update pets.py

--- petshop/pets.py
+++ petshop/pets.py
@@ -1,7 +1,23 @@
+import datetime
+
+from flask import Blueprint
+from flask import render_template, request, redirect, url_for, jsonify
+from flask import g
+
+from . import db
+
+bp = Blueprint("pets", "pets", url_prefix="")
+
+def format_date(d):
+    if d:
+        d = datetime.datetime.strptime(d, '%Y-%m-%d')
+        v = d.strftime("%a - %b %d, %Y")
+        return v
+    else:
+        return None
+
 @bp.route("/search/<field>/<value>")
 def search(field, value):
-    # TBD
-    return ""
     conn = db.get_db()
     cursor = conn.cursor()
     oby1 = request.args.get("order_by", "id")
@@ -14,19 +30,15 @@ def search(field, value):
 def dashboard():
     conn = db.get_db()
     cursor = conn.cursor()
-    oby = request.args.get("order_by", "id") # TODO. This is currently not used. 
-
+   
     #to sort by id
     oby1 = request.args.get("order_by", "id")  
     order = request.args.get("order", "asc")
-    if order == "asc":
     if order == "asc" and oby1=="id":
         cursor.execute(f"select p.id, p.name, p.bought, p.sold, s.name from pet p, animal s where p.species = s.id order by p.id")
-    else:
         pets = cursor.fetchall()
     elif oby1=="id":
         cursor.execute(f"select p.id, p.name, p.bought, p.sold, s.name from pet p, animal s where p.species = s.id order by p.id desc")
-    pets = cursor.fetchall()
         pets = cursor.fetchall()
 
     #to sort by name
@@ -64,11 +76,35 @@ def dashboard():
     elif oby5=="species":
         cursor.execute(f"select p.id, p.name, p.bought, p.sold, s.name from pet p, animal s where p.species = s.id order by s.name desc")
         pets = cursor.fetchall()
-
+    
     return render_template('index.html', pets = pets, order="desc" if order=="asc" else "asc")
 
 
-@@ -63,6 +108,7 @@ def edit(pid):
+@bp.route("/<pid>")
+def pet_info(pid): 
+    conn = db.get_db()
+    cursor = conn.cursor()
+    cursor.execute("select p.name, p.bought, p.sold, p.description, s.name from pet p, animal s where p.species = s.id and p.id = ?", [pid])
+    pet = cursor.fetchone()
+    cursor.execute("select t.name from tags_pets tp, tag t where tp.pet = ? and tp.tag = t.id", [pid])
+    tags = (x[0] for x in cursor.fetchall())
+    name, bought, sold, description, species = pet
+    data = dict(id = pid,
+                name = name,
+                bought = format_date(bought),
+                sold = format_date(sold),
+                description = description, #TODO Not being displayed
+                species = species,
+                tags = tags)
+    return render_template("petdetail.html", **data)
+
+@bp.route("/<pid>/edit", methods=["GET", "POST"])
+def edit(pid):
+    conn = db.get_db()
+    cursor = conn.cursor()
+    if request.method == "GET":
+        cursor.execute("select p.name, p.bought, p.sold, p.description, s.name from pet p, animal s where p.species = s.id and p.id = ?", [pid])
+        pet = cursor.fetchone()
         cursor.execute("select t.name from tags_pets tp, tag t where tp.pet = ? and tp.tag = t.id", [pid])
         tags = (x[0] for x in cursor.fetchall())
         name, bought, sold, description, species = pet
@@ -76,15 +112,14 @@ def dashboard():
         data = dict(id = pid,
                     name = name,
                     bought = format_date(bought),
-@@ -74,10 +120,25 @@ def edit(pid):
+                    sold = format_date(sold),
+                    description = description,
+                    species = species,
+                    tags = tags)
+        return render_template("editpet.html", **data)
     elif request.method == "POST":
         description = request.form.get('description')
         sold = request.form.get("sold")
-        # TODO Handle sold
-        return redirect(url_for("pets.pet_info", pid=pid), 302)
-
-
-
         cursor.execute("select p.sold from pet p where p.id = ?", [pid])
         pet_sold = cursor.fetchone()
         sold_date, = pet_sold
@@ -94,7 +129,7 @@ def dashboard():
             data = (description, '', pid)
             cursor.execute(sql_update_query, data)    
             conn.commit()
-
+        
         if sold_date=='' and sold:
             today = datetime.date.today().strftime('%Y-%m-%d')
             data = (description, today, pid)
